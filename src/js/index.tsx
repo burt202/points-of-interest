b6833255c5e365677e1e3a4187fbf904e6fdc544
Aaron Burtnyk <aaron.burtnyk@candide.com>
2022-08-30 20:56:59 +0100

    Initial commit

---
+++ src/js/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+import * as ReactDom from "react-dom"
+
+import App from "./app"
+
+import "../style.css"
+
+ReactDom.render(<App />, document.body.querySelector("#container"))
